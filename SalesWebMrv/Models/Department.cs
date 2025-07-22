686dfb71dd0f937366f60c2c3ef7833ae2ac5c10
evandropolopw <107819118+evandropolopw@users.noreply.github.com>
2025-07-22 20:54:37 -0300

    Criação do departamento

---
+++ SalesWebMrv/Models/Department.cs
@@ -0,0 +1,11 @@
+﻿
+namespace SalesWebMrv.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+    }
+}
---
+++ SalesWebMrv/Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SalesWebMrv.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
