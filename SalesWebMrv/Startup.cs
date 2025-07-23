07e1c76de273264867285b5e19a0c9281cc2896d
evandropolopw <107819118+evandropolopw@users.noreply.github.com>
2025-07-22 22:08:28 -0300

    Feito o CRUD e criado a tabela departamento

--- SalesWebMrv/Startup.cs
+++ SalesWebMrv/Startup.cs
@@ -37,7 +37,8 @@ namespace SalesWebMrv
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SalesWebMrvContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMrvContext")));
+                    options.UseMySql(Configuration.GetConnectionString("SalesWebMrvContext"), builder =>
+                    builder.MigrationsAssembly("SalesWebMrv")));    
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
