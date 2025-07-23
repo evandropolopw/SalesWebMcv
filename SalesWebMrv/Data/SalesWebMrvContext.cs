bac14995dc125f8a5b30d3a13db2d76fe7c4186d
evandropolopw <107819118+evandropolopw@users.noreply.github.com>
2025-07-22 21:07:53 -0300

    Feito do CRUD

---
+++ SalesWebMrv/Data/SalesWebMrvContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMrv.Models;
+
+namespace SalesWebMrv.Data
+{
+    public class SalesWebMrvContext : DbContext
+    {
+        public SalesWebMrvContext (DbContextOptions<SalesWebMrvContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMrv.Models.Department> Department { get; set; }
+    }
+}
--- SalesWebMrv/Startup.cs
+++ SalesWebMrv/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMrv.Data;
 
 namespace SalesWebMrv
 {
@@ -33,6 +35,9 @@ namespace SalesWebMrv
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalesWebMrvContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMrvContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
