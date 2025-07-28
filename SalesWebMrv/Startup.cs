1a9164ce9fa87b6532e957f10d149a22373cc7b6
evandropolopw <107819118+evandropolopw@users.noreply.github.com>
2025-07-28 07:31:13 -0300

    Incluido o controlador Sellers

--- SalesWebMrv/Startup.cs
+++ SalesWebMrv/Startup.cs
@@ -38,15 +38,18 @@ namespace SalesWebMrv
 
             services.AddDbContext<SalesWebMrvContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("SalesWebMrvContext"), builder =>
-                    builder.MigrationsAssembly("SalesWebMrv")));    
+                    builder.MigrationsAssembly("SalesWebMrv")));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService )
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
