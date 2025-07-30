c2da9587a8d044b6327b6bb882b9d9b7ebcfe179
evandropolopw <107819118+evandropolopw@users.noreply.github.com>
2025-07-30 06:54:37 -0300

    Chave estrangeira idDepartment na Seller

--- SalesWebMrv/Migrations/20250730094103_DepartmentForeignKey.Designer.cs
+++ SalesWebMrv/Migrations/20250730094103_DepartmentForeignKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalesWebMrv.Data;
 
 namespace SalesWebMrv.Migrations
 {
     [DbContext(typeof(SalesWebMrvContext))]
-    partial class SalesWebMrvContextModelSnapshot : ModelSnapshot
+    [Migration("20250730094103_DepartmentForeignKey")]
+    partial class DepartmentForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
