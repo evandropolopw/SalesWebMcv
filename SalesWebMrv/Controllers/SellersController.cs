78f1fcad14e75bf23cbb4d4a1942bc9834cff5e8
evandropolopw <107819118+evandropolopw@users.noreply.github.com>
2025-07-31 07:53:39 -0300

    Detalhes com inner join em departamento

--- SalesWebMrv/Controllers/SellersController.cs
+++ SalesWebMrv/Controllers/SellersController.cs
@@ -64,5 +64,21 @@ namespace SalesWebMrv.Controllers
             _sellerService.Remove(Id);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
     }
 }
